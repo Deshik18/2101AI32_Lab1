a2884737573c739418e0e78175359b0e2f70c3c5
Deshik18 <110047070+Deshik18@users.noreply.github.com>
2022-07-27 22:16:49 +0530

    Update 2.C

--- 2.C
+++ 2.C
@@ -82,15 +82,65 @@ void selectionSort(int *A, int n){
     }
 }
 
+void merge(int A[], int mid, int low, int high)
+{
+    int i, j, k, B[100];
+    i = low;
+    j = mid + 1;
+    k = low;
+
+    while (i <= mid && j <= high)
+    {
+        if (A[i] < A[j])
+        {
+            B[k] = A[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            B[k] = A[j];
+            j++;
+            k++;
+        }
+    }
+    while (i <= mid)
+    {
+        B[k] = A[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        B[k] = A[j];
+        k++;
+        j++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        A[i] = B[i];
+    }
+    
+}
+
+void mergeSort(int A[], int low, int high){
+    int mid; 
+    if(low<high){
+        mid = (low + high) /2;
+        mergeSort(A, low, mid);
+        mergeSort(A, mid+1, high);
+        merge(A, mid, low, high);
+    }
+}
+
 int main(){
-    int A[] = {12, 54, 65, 7, 23, 9};
-    int n = 6;
+    int A[] = {9, 1, 4, 14, 4, 15, 6};
+    int n = 7;
     printArray(A, n);
+    mergeSort(A, 0, 6);
     insertionSort(A, n);
     bubbleSort(A, n); // Function to sort the array
     selectionSort(A, n);
     printArray(A, n);
     return 0;
 }
-
-
