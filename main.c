a8dc8fb28d05a5c1f850c8114ff29359dd8e82e1
Deshik18 <110047070+Deshik18@users.noreply.github.com>
2022-07-28 03:20:56 +1100

    Add files via upload
    Now we have added all operations

--- main.c
+++ main.c
@@ -1,17 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float addNumber(float x, float y){
+    return x+y;
+}
+
+float subNumbers(float x, float y){
+    return x-y;
+}
+
+float multiply(float x, float y){
+    return x*y;
+}
+
 float divide(float x, float y){
     return x/y;
 }
 
+
+
 int main()
 {
     float x,y;
-    printf("Enter two numbers for division : ");
+    int z;
+    printf("Enter two numbers : ");
     scanf("%f",&x);
     scanf("%f",&y);
 
-    printf("The division of two numbers is %f",divide(x,y));
+    printf("Enter\n1 for addition\n2 for subtraction\n3 for multiplication\n4 for division\n");
+    scanf("%d",&z);
+
+    switch(z){
+    case 1:
+        printf("The result of operation is %f",addNumber(x,y));
+        break;
+    case 2:
+        printf("The result of operation is %f",subNumbers(x,y));
+        break;
+    case 3:
+        printf("The result of operation is %f",multiply(x,y));
+        break;
+    case 4:
+        printf("The result of operation is %f",divide(x,y));
+        break;
+    default :
+        printf("Enter valid input");
+    }
     return 0;
 }
