6810045212af47a8f8bb89dd7edc7b349fd157f3
Deshik18 <110047070+Deshik18@users.noreply.github.com>
2022-07-28 03:19:41 +1100

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float subNumbers(float x, float y){
-    return x-y;
+float divide(float x, float y){
+    return x/y;
 }
 
 int main()
 {
     float x,y;
-    printf("Enter two numbers for subtraction : ");
+    printf("Enter two numbers for division : ");
     scanf("%f",&x);
     scanf("%f",&y);
 
-    printf("The difference of two numbers is %f",subNumbers(x,y));
+    printf("The division of two numbers is %f",divide(x,y));
     return 0;
 }
