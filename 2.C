aced8286fd107bb30d97df736bd313a59040d3f2
Deshik18 <110047070+Deshik18@users.noreply.github.com>
2022-07-27 22:04:39 +0530

    Update 2.C

--- 2.C
+++ 2.C
@@ -24,12 +24,53 @@ void insertionSort(int *A, int n){
     }
 }
 
+void bubbleSort(int *A, int n){
+    int temp;
+    int isSorted = 0;
+    for (int i = 0; i < n-1; i++) // For number of pass
+    {
+        printf("Working on pass number %d\n", i+1); 
+        for (int j = 0; j <n-1-i ; j++) // For comparison in each pass
+        {
+            if(A[j]>A[j+1]){
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp; 
+            }
+        }  
+    } 
+}
+ 
+void bubbleSortAdaptive(int *A, int n){
+    int temp;
+    int isSorted = 0;
+    for (int i = 0; i < n-1; i++) // For number of pass
+    {
+        printf("Working on pass number %d\n", i+1);
+        isSorted = 1;
+        for (int j = 0; j <n-1-i ; j++) // For comparison in each pass
+        {
+            if(A[j]>A[j+1]){
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
+                isSorted = 0;
+            }
+        } 
+        if(isSorted){
+            return;
+        }
+    } 
+}
+
 int main(){
     int A[] = {12, 54, 65, 7, 23, 9};
     int n = 6;
     printArray(A, n);
     insertionSort(A, n);
+    bubbleSort(A, n); // Function to sort the array
     printArray(A, n);
     return 0;
 }
+
 
